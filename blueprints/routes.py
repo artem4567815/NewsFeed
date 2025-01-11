e72f68ceb85b3241c50f3b610efc95b9d85deddf
artemturnin <artemiy_08@icloud.com>
2025-01-12 02:41:27 +0300

    Подгрузка постов из бд

--- blueprints/routes.py
+++ blueprints/routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template
-from methods import get_all_news_files
+from methods import get_all_news_files, get_all_news
 
 main_routes = Blueprint('routes', __name__)
 
 @main_routes.route("/events")
 def events():
-    news_list = get_all_news_files()
+    #news_list = get_all_news_files()
+    news_list = get_all_news()
     return render_template("index.html", news=news_list, page_type="events")
 
 @main_routes.route("/wall")
