cd7976f27ad3dbe4bdafe16c21e4d1025454019b
artemturnin <artemiy_08@icloud.com>
2025-04-10 21:14:07 +0300

    дополнил опенапи, переписал тесты так, чтобы клиент тоже мог создавать посты

--- backend/blueprints/admin.py
+++ backend/blueprints/admin.py
@@ -1,26 +1,9 @@
 from flask import Blueprint, request
 from methods import *
-from flask_jwt_extended import jwt_required, get_jwt_identity
-from flask_pydantic import validate
-from schemas import *
+from flask_jwt_extended import jwt_required
 from models import News
 
 admin_routes = Blueprint('admin', __name__)
-
-
-@admin_routes.route('/create/post', methods=['POST'])
-@safe("blueprints/admin.py | save_news")
-@jwt_required()
-@check_jwt_access
-@validate()
-def save_news(body: CreateNewsRequest):
-    file = save_base64_image(body.post_img)
-    user_id = get_jwt_identity()
-
-    news = create_news(body.title, body.short_content, body.content, file,
-                       body.start_date, body.end_date, user_id, body.type, body.tags)
-
-    return jsonify(news.as_dict()), 201
 
 
 @admin_routes.route('/moderation', methods=['GET'])
--- backend/blueprints/posts.py
+++ backend/blueprints/posts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from methods import *
 from flask_pydantic import validate
@@ -24,6 +24,25 @@ def detailed_news(post_id):
     if not news:
         return jsonify({"Message": "Новость не найдена"}), 404
     return jsonify(news.as_dict()), 200
+
+
+@posts.route('/create/post', methods=['POST'])
+@safe("blueprints/admin.py | save_news")
+@jwt_required()
+@validate()
+def save_news(body: CreateNewsRequest):
+    file = save_base64_image(body.post_img)
+    user_id = get_jwt_identity()
+    access = get_jwt()['is_admin']
+
+    if access:
+        news = create_news(body.title, body.short_content, body.content, file,
+                           body.start_date, body.end_date, user_id, body.type, body.tags)
+    else:
+        news = create_news(body.title, body.short_content, body.content, file,
+                           body.start_date, body.end_date, user_id, body.type, body.tags, "draft")
+
+    return jsonify(news.as_dict()), 201
 
 
 @posts.route('/create/wallpapers', methods=['POST'])
