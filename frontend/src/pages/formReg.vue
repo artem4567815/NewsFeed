f284323c365929cc65b4b88209a97b8602f74421
artemturnin <artemiy_08@icloud.com>
2025-04-19 01:21:23 +0300

    fix

--- frontend/src/pages/formReg.vue
+++ frontend/src/pages/formReg.vue
@@ -322,6 +322,7 @@ const submitForm = async () => {
   errorMessage.value = "";
 
   try {
+    console.log(`${import.meta.env.VITE_BASE_URL}/auth/register/client`)
     const registerResponse = await axios.post(
         `${import.meta.env.VITE_BASE_URL}/auth/register/client`,
         register.value,
