2c9da41c18b6bed27a80a4a7a6b3acecbfc615be
artemturnin <artemiy_08@icloud.com>
2025-04-24 00:29:59 +0300

    fix

--- backend/start.sh
+++ backend/start.sh
@@ -12,6 +12,7 @@ python -m flask db init
 python -m flask db migrate
 python -m flask db upgrade
 
+echo $FLASK_DEBUG
 if [ "$FLASK_DEBUG" == "1" ]; then
         python -m flask run --host=0.0.0.0 --port=8080
 else
@@ -19,3 +20,5 @@ else
 fi
 
 exec "$@"
+
+!/bin/sh
