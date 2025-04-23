e64b98607a7b06a71ecc211920e7980fd1ef9945
artemturnin <artemiy_08@icloud.com>
2025-04-23 23:47:16 +0300

    wsgi add

--- backend/start.sh
+++ backend/start.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+# ░░░░ЗАПУСКАЕМ░ГУСЕЙ-РАЗВЕДЧИКОВ░░░░
+# ░░░░░▄▀▀▀▄░░░▄▀▀▀▀▄░░░▄▀▀▀▄░░░░░
+# ▄███▀░◐░░░▌░▐0░░░░0▌░▐░░░◐░▀███▄
+# ░░░░▌░░░░░▐░▌░▐▀▀▌░▐░▌░░░░░▐░░░░
+# ░░░░▐░░░░░▐░▌░▌▒▒▐░▐░▌░░░░░▌░░░░
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
 python -m flask db init
 python -m flask db migrate
 python -m flask db upgrade
 
-python -m flask run --host=0.0.0.0 --port=8080
+if [ "$FLASK_DEBUG" == "1" ]; then
+        python -m flask run --host=0.0.0.0 --port=8080
+else
+        gunicorn -w 4 -b 0.0.0.0:8080 wsgi:app
+fi
 
 exec "$@"
