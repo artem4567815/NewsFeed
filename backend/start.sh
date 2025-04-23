6e31f8bb03ac686961259f400d57fcd5ee56dca4
artemturnin <artemiy_08@icloud.com>
2025-04-24 00:53:40 +0300

    fix

--- backend/start.sh
+++ backend/start.sh
@@ -16,7 +16,7 @@ echo $FLASK_DEBUG
 if [ "$FLASK_DEBUG" == "1" ]; then
         python -m flask run --host=0.0.0.0 --port=8080
 else
-        gunicorn -w 2 -b 0.0.0.0:8080 wsgi:app
+        gunicorn -w 2 -b 0.0.0.0:8080 wsgi:app --timeout 90
 fi
 
 exec "$@"
