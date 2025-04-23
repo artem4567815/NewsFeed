56aa3926c30924b8e34d72ba8c898d76ee0a852d
artemturnin <artemiy_08@icloud.com>
2025-04-24 01:01:29 +0300

    test

--- backend/start.sh
+++ backend/start.sh
@@ -1,24 +1,35 @@
+##!/bin/sh
+#
+## ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+## ░░░░ЗАПУСКАЕМ░ГУСЕЙ-РАЗВЕДЧИКОВ░░░░
+## ░░░░░▄▀▀▀▄░░░▄▀▀▀▀▄░░░▄▀▀▀▄░░░░░
+## ▄███▀░◐░░░▌░▐0░░░░0▌░▐░░░◐░▀███▄
+## ░░░░▌░░░░░▐░▌░▐▀▀▌░▐░▌░░░░░▐░░░░
+## ░░░░▐░░░░░▐░▌░▌▒▒▐░▐░▌░░░░░▌░░░░
+## ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+#
+#python -m flask db init
+#python -m flask db migrate
+#python -m flask db upgrade
+#
+#echo $FLASK_DEBUG
+#if [ "$FLASK_DEBUG" == "1" ]; then
+#        python -m flask run --host=0.0.0.0 --port=8080
+#else
+#        gunicorn -w 2 -b 0.0.0.0:8080 wsgi:app --timeout 90
+#fi
+#
+#exec "$@"
+#
 #!/bin/sh
 
-# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-# ░░░░ЗАПУСКАЕМ░ГУСЕЙ-РАЗВЕДЧИКОВ░░░░
-# ░░░░░▄▀▀▀▄░░░▄▀▀▀▀▄░░░▄▀▀▀▄░░░░░
-# ▄███▀░◐░░░▌░▐0░░░░0▌░▐░░░◐░▀███▄
-# ░░░░▌░░░░░▐░▌░▐▀▀▌░▐░▌░░░░░▐░░░░
-# ░░░░▐░░░░░▐░▌░▌▒▒▐░▐░▌░░░░░▌░░░░
-# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+#!/bin/sh
 
 python -m flask db init
 python -m flask db migrate
 python -m flask db upgrade
 
-echo $FLASK_DEBUG
-if [ "$FLASK_DEBUG" == "1" ]; then
-        python -m flask run --host=0.0.0.0 --port=8080
-else
-        gunicorn -w 2 -b 0.0.0.0:8080 wsgi:app --timeout 90
-fi
+python -m flask run --host=0.0.0.0 --port=8080
 
 exec "$@"
-
-!/bin/sh
